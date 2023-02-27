96d089852090bee56c95a07a1a1753e297e7bdd8
AllanVemy27 <vemyallan8@gmail.com>
2023-02-27 09:48:16 +0100

    Update index.js

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -4,6 +4,74 @@
 //Modale
 
 
+// requete fetch
+if (window.fetch) {
+    // executer requete fetch ici
+} else {
+    // lancer requete XMLHttpRequest
+}
+
+const myImage = document.querySelector('img');
+
+fetch('flowers.jpg')
+.then(function(response) {
+  return response.blob();
+})
+.then(function(myBlob) {
+  const objectURL = URL.createObjectURL(myBlob);
+  myImage.src = objectURL;
+});
+
+var myHeaders = new Headers();
+
+var myInit = { method: 'GET',
+               headers: myHeaders,
+               mode: 'cors',
+               cache: 'default' };
+
+fetch('flowers.jpg',myInit)
+.then(function(response) {
+  return response.blob();
+})
+.then(function(myBlob) {
+  var objectURL = URL.createObjectURL(myBlob);
+  myImage.src = objectURL;
+});
+
+fetch('flowers.jpg').then(function(response) {
+  if(response.ok) {
+    response.blob().then(function(myBlob) {
+      var objectURL = URL.createObjectURL(myBlob);
+      myImage.src = objectURL;
+    });
+  } else {
+    console.log('Mauvaise réponse du réseau');
+  }
+})
+.catch(function(error) {
+  console.log('Il y a eu un problème avec l\'opération fetch : ' + error.message);
+});
+
+var myHeaders = new Headers();
+
+var myInit = { method: 'GET',
+               headers: myHeaders,
+               mode: 'cors',
+               cache: 'default' };
+
+var myRequest = new Request('flowers.jpg',myInit);
+
+fetch(myRequest,myInit)
+.then(function(response) {
+  return response.blob();
+})
+.then(function(myBlob) {
+  var objectURL = URL.createObjectURL(myBlob);
+  myImage.src = objectURL;
+});
+
+var anotherRequest = new Request(myRequest,myInit);
+
 
 
 
@@ -28,11 +96,3 @@ formEl.addEventListener('submit',() => {
     .then(result => console.log(result))
     .catch(error => console.log(error));
 })
-
-// Programmation Connexion / Déconnexion
-if (email && password true) {
-    window.location.href='./index.html';
-} 
-else (email && password false){
-    console.log("Erreur dans l'identifiant ou le mot de passe");
-}
