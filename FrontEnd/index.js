ae35bf73851c744737e89298a8667c9724013e50
AllanVemy27 <vemyallan8@gmail.com>
2023-02-28 11:03:23 +0100

    maj

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -28,3 +28,50 @@ fetch('http://localhost:5678/api-docs/', {
     container.innerHTML = JSON.stringify(data);
 })
 .catch(error => console.error(error));
+
+// Automatisation demande de token
+// Définir les informations d'identifiacation de l'utilisateur
+const username = "utilisateur";
+const password = "motdepasse";
+
+//Envoyer une demande d'authentification à l'API
+fetch('http://localhost:5678/api-docs/'), {
+	method: "POST",
+	body: JSON.stringify({ username, password }),
+	headers: { "Content-Type": "application/json" },
+  }
+	.then((response) => {
+	  if (response.ok) {
+		// Extraire le jeton d'authentification de la réponse de l'API
+		return response.json();
+	  } else {
+		throw new Error("Erreur de demande d'authentification");
+	  }
+	})
+	.then((data) => {
+	  // Stocker le jeton d'authentification dans le stockage local
+	  localStorage.setItem("token", data.token);
+	  console.log("Jeton d'authentification reçu et stocké !");
+	})
+	.catch((error) => {
+	  console.error(error);
+	});
+
+
+
+// Vérification des identifiants de l'utilisateur
+if (identifiantsCorrects) {
+	//Redirection vers la page d'accueil
+	window.location.replace("index.html");
+} else {
+	// Affichage message d'erreur
+	console.log("Identifiants incorrects");
+}
+
+// Stockage du token en local
+localStorage.setItem('token', 'votre_token');
+
+// Récupération du token depuis le stockage local
+const token = localStorage.getItem('token');
+console.log(token); // Affiche "votre_token"
+
